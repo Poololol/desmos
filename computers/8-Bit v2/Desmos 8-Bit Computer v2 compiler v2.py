3580a03a9ff164a5c51cf01ec8cdbf4811af6bef
Braedon <braedonegrillot@gmail.com>
2025-04-02 14:03:31 -0500

    add immediate operations

--- computers/8-Bit v2/Desmos 8-Bit Computer v2 compiler v2.py
+++ computers/8-Bit v2/Desmos 8-Bit Computer v2 compiler v2.py
@@ -1,5 +1,5 @@
 file_path = 'test2.dasm'
-opcodes = {'ADD': '0010','SUB': '0011','XOR': '0100','NOR': '0101','AND': '0110','RSH': '0111'}
+opcodes = {'ADD': '0010','SUB': '0011','XOR': '0100','NOR': '0101','AND': '0110','RSH': '0111', 'LDI': '1000', 'ADI': '1001'}
 definitions = {}
 labels = {}
 with open(file_path, 'r') as inputFile:
@@ -8,9 +8,10 @@ with open(file_path, 'r') as inputFile:
         for line in inputFile:
             if line[0] == '.':
                 labels[line[1:].replace('\n','')] = i
-            elif line[0] == '#':
-                defines = line[8:].replace('\n','').split(' ')
-                definitions[defines[0]] = defines[1]
+                print(labels)
+            elif line[0:8] == '#define ':
+                defined = line[8:].replace('\n','').split(' ')
+                definitions[defined[0]] = defined[1]
                 print(definitions)
             else:
                 opcode = line[:3]
@@ -23,11 +24,23 @@ with open(file_path, 'r') as inputFile:
                         elif operand in definitions:
                             binary.append(format(int(definitions[operand]), '04b'))
                             print(operand, definitions[operand], format(int(definitions[operand]), '04b'))
-                        elif operand in labels:
-                            binary.append(format(int(labels[operand]), '010b'))
-                            print(operand, labels[operand], format(int(labels[operand]), '010b'))
                         else:
                             binary.append(operand)
+                    while len(binary) < 3:
+                        binary.append('0000')
+                    outputFile.write(opcodes[opcode] + ''.join(binary) + '\n')
+                if opcode in ['LDI', 'ADI']:
+                    binary = []
+                    for operand in operands:
+                        if operand.startswith('r') and operand[1].isnumeric():
+                            binary.append(format(int(operand[1:]), '04b'))
+                        elif operand in definitions:
+                            binary.append(format(int(definitions[operand]), '08b'))
+                            print(operand, definitions[operand], format(int(definitions[operand]), '08b'))
+                        else:
+                            immediate = format(int(operand), '08b')
+                            binary.append(immediate[:4])
+                            binary.append(immediate[4:])
                     while len(binary) < 3:
                         binary.append('0000')
                     outputFile.write(opcodes[opcode] + ''.join(binary) + '\n')
