3fb424c543abcd518b2c8cfc4fe6549f3328590c
Poololol <braedonegrillot@gmail.com>
2025-05-01 10:50:38 -0500

    Add more test files

--- computers/8-Bit v2/Desmos 8-Bit Computer v2 compiler v2.py
+++ computers/8-Bit v2/Desmos 8-Bit Computer v2 compiler v2.py
@@ -1,14 +1,19 @@
 import sys
+from bitstring import Bits
 file_path = 'test CAL.dasm'
 if len(sys.argv) > 1:
     file_path = sys.argv[1]
-opcodes = {'NOP': '0000', 'HLT': '0001', 'ADD': '0010', 'SUB': '0011', 'XOR': '0100', 'NOR': '0101', 'AND': '0110', 'RSH': '0111', 
+opcodes = {'NOP': '0000', 'HLT': '0001', 'ADD': '0010', 'SUB': '0011', 'NOR': '0100', 'AND': '0101', 'XOR': '0110', 'RSH': '0111', 
            'LDI': '1000', 'ADI': '1001', 'JMP': '1010', 'BRH': '1011', 'CAL': '1100', 'RET': '1101', 'LOD': '1110', 'STR': '1111'}
+print(opcodes.keys())
 psuedocodes = {'INC': opcodes['ADI'], 'DEC': opcodes['ADI'], 'CMP': opcodes['SUB']}
 psuedocodes2 = {'INC': 1, 'DEC': 255, 'CMP': '0000'}
 definitions = {'zero': '00', 'notzero': '01', 'carry': '10', 'notcarry': '11', 
                '=': '00', '!=': '01', '>=': '10', '<': '11', 
                'z': '00', 'nz': '01', 'c': '10', 'nc': '11'}
+charLookup = {' ': 0, '.': 27, '!': 28, '?':29}
+for i in range(26):
+    charLookup['abcdefghijklmnopqrstuvwxyz'[i]] = i+1
 labels = {}
 with open(file_path, 'r') as inputFile:
     i = 0
@@ -21,15 +26,13 @@ with open(file_path, 'r') as inputFile:
                 defined = line[8:].replace('\n','').split(' ')
                 definitions[defined[0]] = defined[1]
                 print(definitions)
+            elif line[0] in ['\n', ';']:
+                pass
             else:
                 i += 1
         inputFile.seek(0)
         for line in inputFile:
-            if line[0] == '.':
-                continue
-            elif line[0] == '#':
-                continue
-            elif line == '\n':
+            if line[0] in ['.', '#', ';', '\n']:
                 continue
             else:
                 opcode = line[:3]
@@ -69,6 +72,15 @@ with open(file_path, 'r') as inputFile:
                             binary.append(immediate[:4])
                             binary.append(immediate[4:])
                             print(operand, definitions[operand], format(int(definitions[operand]), '08b'))
+                        elif operand.startswith('"'):
+                            immediate = format(charLookup[operand[1].lower()], '08b')
+                            binary.append(immediate[:4])
+                            binary.append(immediate[4:])
+                        elif operand.startswith('0b'):
+                            immediate = format(int(operand, base=2), '08b')
+                            print(immediate)
+                            binary.append(immediate[:4])
+                            binary.append(immediate[4:])
                         else:
                             immediate = format(int(operand), '08b')
                             binary.append(immediate[:4])
@@ -109,10 +121,13 @@ with open(file_path, 'r') as inputFile:
                         if operand.startswith('r') and operand[1].isnumeric():
                             binary.append(format(int(operand[1:]), '04b'))
                         elif operand in definitions:
-                            binary.append(format(int(definitions[operand]), '04b'))
-                            print(operand, definitions[operand], format(int(definitions[operand]), '04b'))
+                            binary.append(Bits(int=int(definitions[operand]), length=4).bin)
+                            print(operand, definitions[operand], Bits(int=int(definitions[operand]), length=4).bin)
+                        elif operand.startswith('"'):
+                            binary.append(format(charLookup[operand[1].lower()], '04b'))
                         else:
-                            binary.append(format(int(operand), '04b'))
+                            #binary.append(format(int(operand), '04b'))
+                            binary.append(Bits(int=int(operand), length=4).bin)
                     if len(binary) < 3:
                         binary.append('0000')
                     #binary.append(format(int(operand[1:]), '04b'))
